7c60b86c506f7ed078c354c901d121b75e431b25
markheckmann <heckmann.mark@gmail.com>
2016-01-31 16:05:17 +0100

    dev stuff

--- dev/search_and_replace.R
+++ dev/search_and_replace.R
@@ -1,5 +1,6 @@
 
 # add graphic to slides with matching and remoce text
+library(R2PPTaddons)
 
 ppt <- PPT.Open("inst/template.pptx", method="RDCOMClient")
 
@@ -9,5 +10,6 @@ PPT.ReplaceTextByGraphic(ppt, what, file = "inst/image_1.png")
 # Note that the text appears twice and the graphic is inserted twice with a warning.
 what <- "[[tag 2]]"
 PPT.ReplaceTextByGraphic(ppt, what, file = "inst/image_2.png")
+
 
 
