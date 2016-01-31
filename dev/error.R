e414193ad0a63fbc868ae5b499785c65cbdb3f7d
markheckmann <heckmann.mark@gmail.com>
2016-01-31 16:05:34 +0100

    more dev stuff

--- dev/error.R
+++ dev/error.R
@@ -1,7 +1,6 @@
 
 library(gap)
 library(R2PPTaddons)
-#library(R.utils)
 library(stringr)
 
 
@@ -11,7 +10,7 @@ file.out <- str_replace(file.template, ".pptx$", "_filled.pptx")
 #scale.width <- 90
 e <- 16                                             # m_einrichtung_id
 bb <- c(10, 180)
-b <- 10
+b <- 180
 # e = 16
 # b = 180
 
@@ -35,6 +34,78 @@ i <- 0
   file.exists(file)
   p <- PPT.ReplaceTextByGraphic(p, placeholder, file)
 p$pres
+
+
+
+#### fit image in box ####
+
+# Find box shape with text pattern
+what <- "[[type=barchart b=180]]"
+slides <- p$pres[["Slides"]]
+ss <- slides_retrieve_shapes(slides, what)
+s <- ss[[1]]
+s$Select()
+
+# insert graphic
+file <- "dev/barchart__einrichtung_16__graphic_180.png"
+sld <- s[["Parent"]]        # get shape's slide
+p <- PPT.UpdateCurrentSlide(p, slide=sld)   # to insert graphic on correct slide
+
+
+# get shape size and position properties
+h <- s[["Height"]]
+w <- s[["Width"]]
+left <- s[["Left"]]
+top <- s[["Top"]]
+
+# check if type is the correct one (actually not needed) 
+s[["Type"]] == 1    # MsoShapeType Enumeration:  msoAutoShape = 1 (e.g. rectangle), msoTextBox = 17
+
+# calculate the new position within the box (box)
+
+# options:
+# position: 0-1 (0 top, )
+sld[["Shapes"]]
+PPT.AddGraphicstoSlide2_(p, file, newslide=FALSE,
+                         width=w, height=h, 
+                         x = "left", y = "top",
+                         x.offset=left,
+                         y.offset=top)
+
+
+
+#### sxs ####
+
+# function to get width and height of scaled graphic
+
+# get shape size and position properties
+h <- s[["Height"]]
+w <- s[["Width"]]
+left <- s[["Left"]]
+top <- s[["Top"]]
+
+# function to position image
+width = .9 
+height = .9
+x = "center"
+y = "center"
+x.offset = 0
+z.offset = 0
+
+shp.width
+shp.height
+shp.left
+shp.top
+
+box.width
+box.height
+box.left
+box.right
+
+
+
+
+
 
 
 
