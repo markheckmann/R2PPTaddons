0693773400ffecf0e2d78fb245023379b78cde66
Mark Heckmann <Mark.Heckmann@deutschebahn.com>
2018-06-27 14:44:59 +0200

    add PTT.AddRectangle function

---
+++ inst/examples/PPT.AddRectangleExample.R
@@ -0,0 +1,13 @@
+\dontrun{
+  
+# add textbox to middle of slide
+p <- PPT.Init(visible=T, method = "RDCOMClient")
+p <- PPT.AddBlankSlide(p)
+p <- PPT.AddRectangle(p, height=.4)
+p <- PPT.AddRectangle(p, top = .5, width = .4, height=.45, 
+                      fill="blue", transparency = .8) 
+p <- PPT.AddRectangle(p, top = .5, width = .4, height=.45, left=.55, 
+                      line.type=4, line.color="red", line.size = 3) 
+
+}
+
