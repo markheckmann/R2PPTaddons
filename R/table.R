139039826d0433986d2ae1d441f918f3713073c6
Mark Heckmann <Mark.Heckmann@deutschebahn.com>
2018-07-19 17:49:13 +0200

    add more AddTable functionality

--- R/table.R
+++ R/table.R
@@ -8,8 +8,11 @@
 #' Adding a table to a slide.
 #' 
 #' @export
-#' @param text.align Horizontal alignment of text. \code{1 = left}, 
-#' \code{2= center}, and \code{3 = right}. Google \code{PpParagraphAlignment} 
+#' @param text.h.align Horizontal alignment of text. \code{1 = "left"}, 
+#' \code{2= "center"} (default), and \code{3 = "right"}. Google \code{PpParagraphAlignment} 
+#' enumeration for more options. Eizher a string or a numeric values can be used.
+#' @param text.v.align Vertical alignment of text. \code{1 = "top"}, 
+#' \code{3= "center"} (dafault), and \code{4 = "bottom"}. Google \code{PpParagraphAlignment} 
 #' enumeration for more options.
 #' @param show.actions All actions are visisble by default. Invisible actions 
 #' will often speed things up.
@@ -20,22 +23,62 @@ PPT.AddTable <- function(ppt,
                          df, 
                          width=.9, 
                          height=.9,
-                         head.height =.01,
-                         cell.height = .01,
-                         #cell.width = .02,
+                         header.height = row.height[1],
+                         row.height = 20,
+                         column.width = NA,
                          left = .05,
                          top = .1,
                          font.size = 18,
                          font.bold = 0,
                          font.italic = 0,
                          font.color = "black",
-                         text.align = 1,
-                         colnames = TRUE,   # add coloumn names to table
-                         rownames = FALSE,  # add rownames to table
+                         text.h.align = 2,   # per column
+                         text.v.align = 3,   # per row
+                         header.h.align = text.h.align,    # header column
+                         header.v.align = text.v.align[1], # header row
+                         colnames = TRUE,    # add coloumn names to table
+                         rownames = FALSE,   # add rownames to table
                          newslide=FALSE, 
                          show.actions=TRUE,   # to updates inivisily for speed up
                          maxscale=1)
 {
+  ## check and process parameters
+  
+  # is column width NA (default)?
+  column_width_is_na <- all(is.na(column.width))
+  
+  # recode header.h.align if character
+  if (is.character(header.h.align)) {
+    header.h.align <- match.arg(tolower(header.h.align), c('left', 'center', 'right'), TRUE)
+    header.h.align <- recode(header.h.align, left = 1, center = 2, right =3)
+    if (any(is.na(header.h.align)))
+      stop("'header.h.align' requires numeric values",
+           " or one of 'left', 'center', 'right'", call. = FALSE)
+  }
+  # recode text.v.align if character
+  if (is.character(text.h.align)) {
+    text.h.align <- match.arg(tolower(text.h.align), c('left', 'center', 'right'), TRUE)
+    text.h.align <- recode(text.h.align, left = 1, center = 2, right =3)
+    if (any(is.na(text.h.align)))
+      stop("'text.h.align' requires numeric values",
+           " or one of 'left', 'center', 'right'", call. = FALSE)
+  }
+  # recode text.v.align if character
+  if (is.character(text.v.align)) {
+    text.v.align <- match.arg(tolower(text.v.align), c('top', 'center', 'bottom'), TRUE)
+    text.v.align <- recode(text.v.align, top = 1, center = 3, bottom =4)
+    if (any(is.na(text.v.align)))
+      stop("'text.v.align' requires numeric values",
+           " or one of 'top', 'center', 'bottom'", call. = FALSE)
+  }
+  # recode header.v.align if character
+  if (is.character(header.v.align)) {
+    header.v.align <- match.arg(tolower(header.v.align), c('top', 'center', 'bottom'))
+    header.v.align <- recode(header.v.align, top = 1, center = 3, bottom =4)
+    if (any(is.na(header.v.align)))
+      stop("'header.v.align' requires a numeric value",
+           " or one of 'top', 'center', 'bottom'", call. = FALSE)
+  }
 
   # get width / height of slides
   sld.width = slide_width(ppt)
@@ -66,31 +109,54 @@ PPT.AddTable <- function(ppt,
   nr <- nrow(df)
   nc <- ncol(df)
   
+  # if no column width is suplied use width of table
+  if (column_width_is_na) column.width <- width / nc
+    
   # recycle vectors to match nrows / ncols
   # columns
-  text.align <- rep_len(text.align, nc)
+  text.h.align <- rep_len(text.h.align, nc)
+  header.h.align <- rep_len(header.h.align, nc)
+  column.width <- rep_len(column.width, nc)
   font.size <- rep_len(font.size, nc)
   font.bold <- rep_len(font.bold, nc)
   font.italic <- rep_len(font.italic, nc)
   font.color <- rep_len(font.color, nc)
+  
   # rows
   
-  # TODO: cell.height now yet vectorized or used
-  # best approach: use cell height and overwrite with 
-  # header.height if header is present
-  cell.height <- rep_len(cell.height, nr)
+  # initialize row heights as identical for all rows
+  row.height <- rep_len(row.height, nr)
+  text.v.align <- rep_len(text.v.align, nr)
+  
+  # replace height of first row if columns names are shown
+  if (colnames) {
+    row.height[1] <- header.height
+    text.v.align[1] <- header.v.align 
+  }
+  
+  # convert all row.heights as fraction of slide height
+  row.height <- ifelse(row.height <= maxscale, 
+                       row.height, 
+                       row.height / sld.height)
+  column.width <- ifelse(column.width <= maxscale, 
+                         column.width, 
+                         column.width / sld.width)
   
   # size and position
-  width_px <- width * sld.width
-  height_px <- sum(cell.height * sld.height)
+  row.height <- row.height * sld.height
+  column.width <- column.width * sld.width
+  width_px <- sum(column.width)
+  height_px <- sum(row.height)
   left_px <- left * sld.width
   top_px <- top * sld.height
   
-  
   # Add empty table
-  shp <- shapes$AddTable(NumRows = nr, NumColumns = nc, 
-                         Left = left_px, Top = top_px, 
-                         Width = width_px, Height = height_px)
+  shp <- shapes$AddTable(NumRows = nr, 
+                         NumColumns = nc, 
+                         Left = left_px, 
+                         Top = top_px, 
+                         Width = width_px, 
+                         Height = height_px)
   
   # hide shape while it is update
   if (!show.actions)
@@ -99,6 +165,7 @@ PPT.AddTable <- function(ppt,
   # fill table with values from dataframe 
   for (i in 1L:nr) {
     for (j in 1L:nc) {
+      ## cell and column actions
       cat("\rrow:", i, "col:", j)
       cell <- shp[["Table"]]$Cell(i, j)
       txt_rng <- cell[["Shape"]][["TextFrame"]][["TextRange"]]
@@ -114,12 +181,32 @@ PPT.AddTable <- function(ppt,
       fc <- f[["Color"]]
       fc[["RGB"]] <- color_to_integer(font.color[j])
       
+      # alignment horiz 
+      if (colnames & i == 1) 
+        h.align <- header.h.align
+      else 
+        h.align <- text.h.align
       p <- txt_rng[["ParagraphFormat"]]
-      p[["Alignment"]] <- text.align[j]
+      p[["Alignment"]] <- h.align[j]
+      # alignment vertical
+      txt_frm <- cell[["Shape"]][["TextFrame"]]
+      txt_frm[["VerticalAnchor"]] <- text.v.align[i]
       
     }
   }
-
+  
+  ## row actions
+  for (i in 1L:nr) {
+    row <- shp[["Table"]][["Rows"]]$Item(i)
+    row[["Height"]] <- row.height[i]
+  }
+  
+  ## column actions
+  for (j in 1L:nc) {
+    column <- shp[["Table"]][["Columns"]]$Item(j)
+    column[["Width"]] <- column.width[j]
+  }
+  
   # TODO:  
   # # set wdiths
   # ActivePresentation.Slides(2).Shapes(5).Table.Columns(1).Width = 80
--- inst/examples/PPT.AddTableExample.R
+++ inst/examples/PPT.AddTableExample.R
@@ -6,44 +6,65 @@ p <- PPT.Init(visible=T, method = "RDCOMClient")
 #### EXAMPLE 1 ####
 
 p <- PPT.AddBlankSlide(p)
-m <- mtcars[1:2, ]
+m2 <- mtcars[1:2, ]
+m4 <- mtcars[1:4, ]
+m10 <- mtcars[1:10, ]
 
 # no column names
-p <- PPT.AddTable(p, m, colnames = F)
+p <- PPT.AddTable(p, m2, colnames = F)
 # change position and font size
-p <- PPT.AddTable(p, m, top=.25, font.size=12)
-p <- PPT.AddTable(p, m, colnames = F, rownames = T, top=.45, font.color="red")
-p <- PPT.AddTable(p, m, rownames = T, top=.7)
+p <- PPT.AddTable(p, m2, top=.25, font.size=12)
+p <- PPT.AddTable(p, m2, colnames = F, rownames = T, top=.45, font.color="red")
+p <- PPT.AddTable(p, m2, rownames = T, top=.7)
 
-#### EXAMPLE: FONTS ####
-
-p <- PPT.AddBlankSlide(p)
+#### FONTS ####
 
 # font and text arguments are vectorized and will be recycled 
 # to fit the number of columns
-p <- PPT.AddTable(p, m, 
+p <- PPT.AddBlankSlide(p)
+p <- PPT.AddTable(p, m4, 
                   text.align = 1:3, 
                   font.color=c("cyan3", "#4400ff"),
                   font.bold = c(F,F,T), 
                   font.italic = c(T,F),
                   font.size = c(10,14,18))
 
-p <- PPT.AddTable(p, m, cell.height = .01)
+#### ROW HEIGHT AND COLUMN WIDTH ####
+
+# big header, smaller rows
+p <- PPT.AddTable(p, m4, header.height = 80, row.height = 20)
+# alternating small and big row heights
+p <- PPT.AddTable(p, m4, header.height = 80, row.height = c(20, 60))
+# big and small columns
+p <- PPT.AddTable(p, m4, column.width = c(40, 80))
+
+
+#### TEXT ALIGNMENT  ####
+
+# different vertical alignments for header and rows
+p <- PPT.AddTable(p, m4,
+                  header.v.align = "top", 
+                  text.v.align = "bottom",
+                  row.height = 60)
+# alternate aligment of rows
+p <- PPT.AddTable(p, m4,
+                  header.v.align = "top", 
+                  text.v.align = c("top", "bottom"),   # alternate rows
+                  row.height = 60)
+# different horizontal alignments for columns
+p <- PPT.AddTable(p, m4,
+                  header.h.align = c("right","left"), 
+                  text.h.align = c("left", "right"),   # aternate columns
+                  row.height = 60)
+
 
 #### MISC EXAMPLES  ####
 
 #~ 3 x speed up when setting show.actions to FALSE
-
-m <- mtcars[1:10, ]
 p <- PPT.AddBlankSlide(p)
-system.time(
-  p <- PPT.AddTable(p, m)
-)
-
+system.time( p <- PPT.AddTable(p, m10) )
 p <- PPT.AddBlankSlide(p)
-system.time(
-  p <- PPT.AddTable(p, m, show.actions=F)
-)
+system.time( p <- PPT.AddTable(p, m10, show.actions=F) )
 
 }
 
